8927ce95eaaefa3df0b1205876e0e8dc439eb159
Nikolay Nikolaev <evrinoma@gmail.com>
2019-04-23 09:44:09 +0300

    refactroing slave project

--- MAIN/I2C_SLAVE/I2C_SLAVE.v
+++ MAIN/I2C_SLAVE/I2C_SLAVE.v
@@ -1,9 +1,7 @@
 //use only to load on board Chip
 `define WITH_DEBOUNCE
-`undef WITH_DEBOUNCE
-
-`define FULL_QUERY_BMP180
-`undef FULL_QUERY_BMP180
+//`undef WITH_DEBOUNCE
+//`define DEBUG_SLAVE_DRIVER
 
 module I2C_MASTER_SLAVE(
 	receive,
@@ -12,16 +10,18 @@ module I2C_MASTER_SLAVE(
 	sended,
 	clk, 
 	reset, 
-	out, 
 	ready, 
 	scl, 
-	sda
+	sda,
+	out,
+	state
 );
 
 input 	wire clk;					//сигнал тактовой частоты
 input 	wire reset;					//сигнал сброса
 
 output	wire [7:0] out;			//данные
+output	wire [7:0] state;			//данные
 output	wire ready;					//готовность контроллера I2C
 
 inout 	sda;							//линия передачи данных I2C 
@@ -31,7 +31,6 @@ wire [7:0] datareceive;
 wire [7:0] datasend;
 wire [6:0] address;
 
-wire[5:0] state;
 output wire receive;
 output wire received;
 output wire send;
@@ -43,29 +42,17 @@ wire slv_sended;
 wire[7:0] slv_datareceive;
 wire slv_received;
 
-wire startIC;
+wire[5:0] startMaster;
+wire[5:0] startSlave;
+wire[5:0] _stateScl;
 
 `ifdef WITH_DEBOUNCE
 	wire resetDeBounce;
-	wire swIdDeBounce;
-	wire swShowDeBounce;
 
 	DEBOUNCE resetKey( 
 	.clk(clk), 
 	.keyBounce(reset), 
 	.keyDeBounce(resetDeBounce)
-	);
-	
-	DEBOUNCE swIdKey( 
-	.clk(clk), 
-	.keyBounce(swId), 
-	.keyDeBounce(swIdDeBounce)
-	);
-	
-	DEBOUNCE swShowKey( 
-	.clk(clk), 
-	.keyBounce(swShow), 
-	.keyDeBounce(swShowDeBounce)
 	);
 `endif
 
@@ -86,24 +73,41 @@ I2C_MASTER I2C_MASTER(
 	.receive(receive), 
 	.datareceive(datareceive), 
 	.received(received), 
-	.state(stateIC)
+	.state(startMaster)
 );
 
 I2C_SLAVE I2C_SLAVE (
 .clk(clk), 
-.reset(reset), 
+`ifdef WITH_DEBOUNCE
+	.reset(resetDeBounce), 
+`else
+	.reset(reset), 
+`endif
 .sda(sda), 
 .scl(scl), 
 .address(address),
 .datasend(slv_datasend), 
 .sended(slv_sended), 
 .datareceive(slv_datareceive), 
-.received(slv_received)
+.received(slv_received),
+.state(state), 
+._stateScl(out)
 );
 
+`ifdef DEBUG_SLAVE_DRIVER
+	wire[6:0] dstate;
+`endif
+
 SLAVE_DRIVER SLAVE_DRIVER (
+`ifdef DEBUG_SLAVE_DRIVER
+	.dstate(state), 
+`endif
 .clk(clk), 
-.reset(reset), 
+`ifdef WITH_DEBOUNCE
+	.reset(resetDeBounce), 
+`else
+	.reset(reset), 
+`endif
 .address(address), 
 .datasend(slv_datasend), 
 .sended(slv_sended), 
